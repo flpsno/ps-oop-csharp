f48350a629567d30fd9a735c9b8276f2d6f81e9b
flpsno <flpsno@hotmail.com>
2018-05-08 19:28:37 -0300

    adicionado customer repository e a classe address.

---
+++ ACM.BL.Test/CustomerRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL.Test
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveExisting()
+        {
+            //-- Arrange
+            var customerRepository = new CustomerRepository();
+            var expect = new Customer(1)
+            {
+                EmailAddress = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins"
+            };
+
+            //-- Act
+            var actual = customerRepository.Retrieve(1);
+
+            //-- Assert
+            //Assert.AreEqual(expect, actual);
+            Assert.AreEqual(expect.CustomerId, actual.CustomerId);
+            Assert.AreEqual(expect.EmailAddress, actual.EmailAddress);
+            Assert.AreEqual(expect.FirstName, actual.FirstName);
+            Assert.AreEqual(expect.LastName, actual.LastName);
+        }
+    }
+}
