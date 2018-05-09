686dd46bad28b1542e7e03e48f87d9cec84ded06
flpsno <flpsno@hotmail.com>
2018-05-09 17:23:01 -0300

    adicionada classes displayorder e displayorderitem.

--- ACM.BL.Test/CustomerRepositoryTest.cs
+++ ACM.BL.Test/CustomerRepositoryTest.cs
@@ -83,5 +83,65 @@ namespace ACM.BL.Test
                 Assert.AreEqual(expeted.AddressList[i].PostalCode, actual.AddressList[i].PostalCode);
             }
         }
+
+        [TestMethod]
+        public void RetrieveOrderDisplayTest()
+        {
+            //-- Arrange
+            var orderRepository = new OrderRepository();
+            var expected = new OrderDisplay()
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins",
+                OrderDate = new DateTimeOffset(2014, 4, 14, 10, 00, 00, new TimeSpan(1, 0, 0)),
+                ShippingAddress = new Address()
+                {
+                    StreetLine1 = "Bag End",
+                    StreetLine2 = "Bagshot row",
+                    City = "Hobbiton",
+                    State = "Shire",
+                    Country = "Middle Earth",
+                    PostalCode = "144"
+                },
+
+                OrderDisplayItemList = new List<OrderDisplayItem>()
+                {
+                    new OrderDisplayItem()
+                    {
+                        ProductName = "Sunflowers",
+                        PuchasePrice = 15.96M,
+                        OrderQuantity = 2
+                    },
+                    new OrderDisplayItem()
+                    {
+                        ProductName = "Rake",
+                        PuchasePrice = 6M,
+                        OrderQuantity = 1
+                    }
+                }
+            };
+
+            //-- Act
+            var actual = orderRepository.RetrieveOrderDisplay(10);
+
+            //-- Assert
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.AreEqual(expected.OrderDate, actual.OrderDate);
+
+            Assert.AreEqual(expected.ShippingAddress.AddressType, actual.ShippingAddress.AddressType);
+            Assert.AreEqual(expected.ShippingAddress.StreetLine1, actual.ShippingAddress.StreetLine1);
+            Assert.AreEqual(expected.ShippingAddress.City, actual.ShippingAddress.City);
+            Assert.AreEqual(expected.ShippingAddress.State, actual.ShippingAddress.State);
+            Assert.AreEqual(expected.ShippingAddress.Country, actual.ShippingAddress.Country);
+            Assert.AreEqual(expected.ShippingAddress.PostalCode, actual.ShippingAddress.PostalCode);
+
+            for (int i = 0; i < 1; i++)
+            {
+                Assert.AreEqual(expected.OrderDisplayItemList[i].OrderQuantity, actual.OrderDisplayItemList[i].OrderQuantity);
+                Assert.AreEqual(expected.OrderDisplayItemList[i].ProductName, actual.OrderDisplayItemList[i].ProductName);
+                Assert.AreEqual(expected.OrderDisplayItemList[i].PuchasePrice, actual.OrderDisplayItemList[i].PuchasePrice);
+            }
+        }
     }
 }
--- ACM.BL/OrderRepository.cs
+++ ACM.BL/OrderRepository.cs
@@ -8,17 +8,59 @@ namespace ACM.BL
 {
     public class OrderRepository
     {
-
         public Order Retrieve(int orderId)
         {
             Order order = new Order();
 
             if (orderId == 10)
             {
-                order.OrderDate = new DateTimeOffset(2008, 5, 1, 8, 6, 32,new TimeSpan(1, 0, 0));
+                order.OrderDate = new DateTimeOffset(2008, 5, 1, 8, 6, 32, new TimeSpan(1, 0, 0));
             }
 
             return order;
+        }
+
+        public OrderDisplay RetrieveOrderDisplay(int orderId)
+        {
+            OrderDisplay orderDisplay = new OrderDisplay();
+
+            if (orderId == 10)
+            {
+                orderDisplay.FirstName = "Bilbo";
+                orderDisplay.LastName = "Baggins";
+                orderDisplay.OrderDate = new DateTimeOffset(2014, 4, 14, 10, 00, 00, new TimeSpan(1, 0, 0));
+                orderDisplay.ShippingAddress = new Address()
+                {
+                    StreetLine1 = "Bag End",
+                    StreetLine2 = "Bagshot row",
+                    City = "Hobbiton",
+                    State = "Shire",
+                    Country = "Middle Earth",
+                    PostalCode = "144"
+                };
+
+                orderDisplay.OrderDisplayItemList = new List<OrderDisplayItem>();
+
+                if (orderId == 10)
+                {
+                    var orderDisplayItem = new OrderDisplayItem()
+                    {
+                        ProductName = "Sunflowers",
+                        PuchasePrice = 15.96M,
+                        OrderQuantity = 2
+                    };
+                    orderDisplay.OrderDisplayItemList.Add(orderDisplayItem);
+
+                    orderDisplayItem = new OrderDisplayItem()
+                    {
+                        ProductName = "Rake",
+                        PuchasePrice = 6M,
+                        OrderQuantity = 1
+                    };
+                    orderDisplay.OrderDisplayItemList.Add(orderDisplayItem);
+                }
+            }
+            return orderDisplay;
         }
 
         public bool Save()
