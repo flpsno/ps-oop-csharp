fbe7cb701309fb23c0eae386781700de83121147
flpsno <flpsno@hotmail.com>
2018-05-09 17:26:59 -0300

    adicionada propriedade customertype na classe customer.

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -22,6 +22,8 @@ namespace ACM.BL
 
         public List<Address> AddressList { get; set; }
 
+        public int CustomerType { get; set; }
+
         public static int InstanceCount { get; set; }
 
         private string _lastName;
