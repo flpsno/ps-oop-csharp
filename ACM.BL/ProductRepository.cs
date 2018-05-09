3e25a7be8af6bc5617db163d8a3100e5f5d51737
flpsno <flpsno@hotmail.com>
2018-05-09 16:12:10 -0300

    Adicionada classes repositorias do product e order.

---
+++ ACM.BL/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class ProductRepository
+    {
+
+        public Product Retrieve(int productId)
+        {
+            Product product = new Product();
+
+            if (productId == 2)
+            {
+                product.ProductName = "Sunflowers";
+                product.ProductDescription = "Assorted Size";
+                product.CurrentPrice = 15.98M;
+            }
+
+            return product;
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+    }
+}
